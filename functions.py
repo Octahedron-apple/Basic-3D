ad73c53bceac930e11e2ad7e8113e5fe08779ecc
Octahedron-apple <bhavya1singh1@gmail.com>
2026-03-06 18:07:08 +0530

    Refactor rotation and translation functions

--- functions.py
+++ functions.py
@@ -3,24 +3,25 @@ import math
 from PIL import Image, ImageDraw
 import os
 import subprocess
-def rotate(points, angle, axis):
-    rad = math.radians(angle)
-    c = math.cos(rad)
-    s = math.sin(rad)
-    if axis.upper() == 'X':
-        rotation_matrix = np.array([
+
+def rotate(ps, a, ax):
+    rd = math.radians(a)
+    c = math.cos(rd)
+    s = math.sin(rd)
+    if ax.upper() == 'X':
+        m = np.array([
             [1, 0, 0],
             [0, c, -s],
             [0, s, c]
         ])
-    elif axis.upper() == 'Y':
-        rotation_matrix = np.array([
+    elif ax.upper() == 'Y':
+        m = np.array([
             [c, 0, s],
             [0, 1, 0],
             [-s, 0, c]
         ])
-    elif axis.upper() == 'Z':
-        rotation_matrix = np.array([
+    elif ax.upper() == 'Z':
+        m = np.array([
             [c, -s, 0],
             [s, c, 0],
             [0, 0, 1]
@@ -28,82 +29,105 @@ def rotate(points, angle, axis):
     else:
         raise ValueError("Axis must be 'X', 'Y', or 'Z'")
 
-    rotated_points = []
-    for point in points:
-        
-        rotated_point = np.dot(rotation_matrix, point)
-        rotated_points.append(rotated_point)
-    return rotated_points
-def translate(points, shift):
+    rp = []
+    for p in ps:
+        np_ = np.dot(m, p)
+        rp.append(np_)
+    return rp
+
+def translate(ps, s):
     tp = []
-    for i in points:
-        np = i + shift 
-        tp.append(np)
-    points=tp  
+    for p in ps:
+        np_ = p + s 
+        tp.append(np_)
     return tp
-def print_points(points):
-    formatted_points = [f"({p[0]},{p[1]},{p[2]})" for p in points]
-    print(",".join(formatted_points))
-def unit_cube(scale):
-    points = []
+
+def print_points(ps):
+    fs = [f"({p[0]},{p[1]},{p[2]})" for p in ps]
+    print(",".join(fs))
+
+def unit_cube(s):
+    ps = []
     for x in [-1, 0, 1]:
         for y in [-1, 0, 1]:
             for z in [-1, 0, 1]:
-               
-                point=np.array([x,y,z])*scale
-                points.append(point)
-                    
-    return points
-def unit_Sphere(scale):
-    points = []
+                p=np.array([x,y,z])*s
+                ps.append(p)
+    return ps
+
+def unit_Sphere(s):
+    ps = []
     for x in range(-2,3):
         for y in range(-2,3):
             for z in range(-2,3):
-                vec = np.array([x, y, z])*0.5
-                magnitude = np.linalg.norm(vec)
-                unit_vec = vec / magnitude
-                point = unit_vec * scale
-                points.append(point)
-    return points
-def threeDtotwoD(points, camera, focal_length): #To be upgraded to accept camera angle aswell
-    relative=[]
-    for i in range(len(points)):
-        relative.append(points[i]-camera)
-    return_points=[]
-    for i in range(len(points)):
-        return_points.append(np.array([focal_length*relative[i][0]/relative[i][2],focal_length*relative[i][1]/relative[i][2]]))
-    return return_points
-def draw_to_image(points, filename="Out.png"):
-    img = Image.new('RGB', (500, 500), 'white')
-    draw = ImageDraw.Draw(img)
-    center_x, center_y = 250, 250
-    for point in points:
-        x=point[0]
-        y=point[1]
-        screen_x = center_x + x
-        screen_y = center_y + y
-        radius = 2
-        draw.ellipse(
-            (screen_x - radius, screen_y - radius, screen_x + radius, screen_y + radius), 
+                v = np.array([x, y, z])*0.5
+                m = np.linalg.norm(v)
+                if m == 0:
+                    continue
+                uv = v / m
+                p = uv * s
+                ps.append(p)
+    return ps
+
+def threeDtotwoD(ps, c, f): 
+    rl=[]
+    for i in range(len(ps)):
+        rl.append(ps[i]-c)
+    rp=[]
+    for i in range(len(ps)):
+        z = rl[i][2]
+        if z == 0: z = 0.001
+        rp.append(np.array([f*rl[i][0]/z,f*rl[i][1]/z]))
+    return rp
+
+def draw_to_image(ps, fn="Out.png"):
+    im = Image.new('RGB', (500, 500), 'white')
+    dw = ImageDraw.Draw(im)
+    cx, cy = 250, 250
+    for p in ps:
+        x=p[0]
+        y=p[1]
+        sx = cx + x
+        sy = cy + y
+        r = 2
+        dw.ellipse(
+            (sx - r, sy - r, sx + r, sy + r), 
             fill='black'
         )
-    img.save(filename)
-def print_points_2D(points):
-    
-    formatted_points = [f"({p[0]},{p[1]})" for p in points]
-    print(",".join(formatted_points))
-def unit_circle(scale,axis):
-    if axis.upper() == 'X':
-        a = [0,0,scale]
-    elif axis.upper() == 'Y':
-        a = [0,0,scale]
-    elif axis.upper() == 'Z':
-        a = [0,scale,0]
+    im.save(fn)
+
+def print_points_2D(ps):
+    fs = [f"({p[0]},{p[1]})" for p in ps]
+    print(",".join(fs))
+
+def unit_circle(s, ax):
+    if ax.upper() == 'X':
+        a = [0,0,s]
+    elif ax.upper() == 'Y':
+        a = [0,0,s]
+    elif ax.upper() == 'Z':
+        a = [0,s,0]
     else:
         raise ValueError("Axis must be 'X', 'Y', or 'Z'")
-    points =[]
+    ps =[]
     for i in range(20):
-        points.append(rotate(a,18,axis))
-    return points
-def strech():
-    
+        ps.append(rotate(a,18,ax))
+    return ps
+
+def strech(ps, ax, s):
+    rp = []
+    for p in ps:
+        x, y, z = p[0], p[1], p[2]
+        if ax.upper() == 'X': x *= s
+        elif ax.upper() == 'Y': y *= s
+        elif ax.upper() == 'Z': z *= s
+        else: raise ValueError("Axis must be 'X', 'Y', or 'Z'")
+        rp.append(np.array([x, y, z]))
+    return rp
+
+def scale(ps, s):
+    rp = []
+    for p in ps:
+        np_ = p * s
+        rp.append(np_)
+    return rp
