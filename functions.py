d9d9e82bf75285d8d88a34216c27c22935820267
Bhavya Singh <bhavya1singh1@gmail.com>
2026-06-07 15:32:32 +0530

    Starting over, removing old code

--- functions.py
+++ functions.py
@@ -4,143 +4,180 @@ from PIL import Image, ImageDraw
 import os
 import subprocess
 
-def rotate(ps, a, ax):
-    rd = math.radians(a)
-    c = math.cos(rd)
-    s = math.sin(rd)
-    if ax.upper() == 'X':
-        m = np.array([
-            [1, 0, 0],
-            [0, c, -s],
-            [0, s, c]
-        ])
-    elif ax.upper() == 'Y':
-        m = np.array([
-            [c, 0, s],
-            [0, 1, 0],
-            [-s, 0, c]
-        ])
-    elif ax.upper() == 'Z':
-        m = np.array([
-            [c, -s, 0],
-            [s, c, 0],
-            [0, 0, 1]
-        ])
-    else:
-        raise ValueError("Axis must be 'X', 'Y', or 'Z'")
+class Point:
+    def __init__(self, Coordinates):
+        self.coords = Coordinates
+    def rotate(self, angle, axis):
+         
 
-    rp = []
-    for p in ps:
-        np_ = np.dot(m, p)
-        rp.append(np_)
-    return rp
+       
+class Obj:
+    def __init__(self, points=None):
+        if points is None:
+            self.points = []
+        else:
+            self.points = points
 
-def translate(ps, s):
-    tp = []
-    for p in ps:
-        np_ = p + s 
-        tp.append(np_)
-    return tp
+    def rotate(self, angle, axis):
+        angle_radians = math.radians(angle)
+        cos_val = math.cos(angle_radians)
+        sin_val = math.sin(angle_radians)
+        
+        if axis.upper() == 'X':
+            rotation_matrix = np.array([
+                [1, 0, 0],
+                [0, cos_val, -sin_val],
+                [0, sin_val, cos_val]
+            ])
+        elif axis.upper() == 'Y':
+            rotation_matrix = np.array([
+                [cos_val, 0, sin_val],
+                [0, 1, 0],
+                [-sin_val, 0, cos_val]
+            ])
+        elif axis.upper() == 'Z':
+            rotation_matrix = np.array([
+                [cos_val, -sin_val, 0],
+                [sin_val, cos_val, 0],
+                [0, 0, 1]
+            ])
+        else:
+            raise ValueError("Axis must be 'X', 'Y', or 'Z'")
 
-def print_points(ps):
-    fs = [f"({p[0]},{p[1]},{p[2]})" for p in ps]
-    print(",".join(fs))
+        result_points = []
+        for point in self.points:
+            coords = point.coords if hasattr(point, 'coords') else point
+            new_coords = np.dot(rotation_matrix, coords)
+            result_points.append(Point(new_coords))
+        self.points = result_points
+        return self
 
-def unit_cube(s):
-    ps = []
-    for x in [-1, 0, 1]:
-        for y in [-1, 0, 1]:
-            for z in [-1, 0, 1]:
-                p=np.array([x,y,z])*s
-                ps.append(p)
-    return ps
+    def translate(self, shift_vector):
+        translated_points = []
+        for point in self.points:
+            coords = point.coords if hasattr(point, 'coords') else point
+            new_coords = coords + shift_vector 
+            translated_points.append(Point(new_coords))
+        self.points = translated_points
+        return self
 
-def unit_Sphere(s):
-    ps = []
-    for x in range(-2,3):
-        for y in range(-2,3):
-            for z in range(-2,3):
-                v = np.array([x, y, z])*0.5
-                m = np.linalg.norm(v)
-                if m == 0:
-                    continue
-                uv = v / m
-                p = uv * s
-                ps.append(p)
-    return ps
+    def print_points(self):
+        formatted_strings = [f"({point[0]},{point[1]},{point[2]})" for point in self.points]
+        print(",".join(formatted_strings))
 
-def unit_cone(s):
-    ps = []
-    for i in range(11):
-        h = i / 10.0
-        r = 1.0 - h
-        for j in range(21):
-            a = j * (math.pi * 2 / 20.0)
-            x = r * math.cos(a)
-            y = r * math.sin(a)
-            p = np.array([x, y, h - 0.5]) * s
-            ps.append(p)
-    return ps
+    def generate_unit_cube(self, scale_factor):
+        self.points = []
+        for x in [-1, 0, 1]:
+            for y in [-1, 0, 1]:
+                for z in [-1, 0, 1]:
+                    point_coords = np.array([x, y, z]) * scale_factor
+                    self.points.append(Point(point_coords))
+        return self
 
-def threeDtotwoD(ps, c, f): 
-    rl=[]
-    for i in range(len(ps)):
-        rl.append(ps[i]-c)
-    rp=[]
-    for i in range(len(ps)):
-        z = rl[i][2]
-        if z == 0: z = 0.001
-        rp.append(np.array([f*rl[i][0]/z,f*rl[i][1]/z]))
-    return rp
+    def generate_unit_sphere(self, scale_factor):
+        self.points = []
+        for x in range(-2, 3):
+            for y in range(-2, 3):
+                for z in range(-2, 3):
+                    vector = np.array([x, y, z]) * 0.5
+                    magnitude = np.linalg.norm(vector)
+                    if magnitude == 0:
+                        continue
+                    unit_vector = vector / magnitude
+                    point_coords = unit_vector * scale_factor
+                    self.points.append(Point(point_coords))
+        return self
 
-def draw_to_image(ps, fn="Out.png"):
-    im = Image.new('RGB', (500, 500), 'white')
-    dw = ImageDraw.Draw(im)
-    cx, cy = 250, 250
-    for p in ps:
-        x=p[0]
-        y=p[1]
-        sx = cx + x
-        sy = cy + y
-        r = 2
-        dw.ellipse(
-            (sx - r, sy - r, sx + r, sy + r), 
-            fill='black'
-        )
-    im.save(fn)
+    def generate_unit_cone(self, scale_factor):
+        self.points = []
+        for i in range(11):
+            height = i / 10.0
+            radius = 1.0 - height
+            for j in range(21):
+                angle = j * (math.pi * 2 / 20.0)
+                x = radius * math.cos(angle)
+                y = radius * math.sin(angle)
+                point_coords = np.array([x, y, height - 0.5]) * scale_factor
+                self.points.append(Point(point_coords))
+        return self
 
-def print_points_2D(ps):
-    fs = [f"({p[0]},{p[1]})" for p in ps]
-    print(",".join(fs))
+    def project_3d_to_2d(self, camera_position, focal_length): 
+        relative_points = []
+        for point in self.points:
+            coords = point.coords if hasattr(point, 'coords') else point
+            relative_points.append(coords - camera_position)
+            
+        projected_points = []
+        for rel_point in relative_points:
+            z = rel_point[2]
+            if z == 0: 
+                z = 0.001
+            projected_points.append(Point([focal_length * rel_point[0] / z, focal_length * rel_point[1] / z]))
+        
+        return Obj(projected_points)
 
-def unit_circle(s, ax):
-    if ax.upper() == 'X':
-        a = [0,0,s]
-    elif ax.upper() == 'Y':
-        a = [0,0,s]
-    elif ax.upper() == 'Z':
-        a = [0,s,0]
-    else:
-        raise ValueError("Axis must be 'X', 'Y', or 'Z'")
-    ps =[]
-    for i in range(20):
-        ps.append(rotate(a,18,ax))
-    return ps
+    def draw_to_image(self, filename="Out.png"):
+        image = Image.new('RGB', (500, 500), 'white')
+        draw = ImageDraw.Draw(image)
+        center_x, center_y = 250, 250
+        
+        for point in self.points:
+            x = point[0]
+            y = point[1]
+            screen_x = center_x + x
+            screen_y = center_y + y
+            radius = 2
+            draw.ellipse(
+                (screen_x - radius, screen_y - radius, screen_x + radius, screen_y + radius), 
+                fill='black'
+            )
+        image.save(filename)
 
-def strech(ps, ax, s):
-    rp = []
-    for p in ps:
-        x, y, z = p[0], p[1], p[2]
-        if ax.upper() == 'X': x *= s
-        elif ax.upper() == 'Y': y *= s
-        elif ax.upper() == 'Z': z *= s
-        else: raise ValueError("Axis must be 'X', 'Y', or 'Z'")
-        rp.append(np.array([x, y, z]))
-    return rp
+    def print_points_2d(self):
+        formatted_strings = [f"({point[0]},{point[1]})" for point in self.points]
+        print(",".join(formatted_strings))
 
-def scale(ps, s):
-    rp = []
-    for p in ps:
-        np_ = p * s
-        rp.append(np_)
-    return rp
+    def generate_unit_circle(self, scale_factor, axis):
+        if axis.upper() == 'X':
+            start_point = [0, 0, scale_factor]
+        elif axis.upper() == 'Y':
+            start_point = [0, 0, scale_factor]
+        elif axis.upper() == 'Z':
+            start_point = [0, scale_factor, 0]
+        else:
+            raise ValueError("Axis must be 'X', 'Y', or 'Z'")
+        
+        temp_obj = Obj([Point(start_point)])
+        self.points = []
+        for _ in range(20):
+            temp_obj.rotate(18, axis)
+            self.points.append(Point(temp_obj.points[0].coords))
+        return self
+
+    def stretch(self, axis, scale_factor):
+        result_points = []
+        for point in self.points:
+            coords = point.coords if hasattr(point, 'coords') else point
+            x, y, z = coords[0], coords[1], coords[2]
+            
+            if axis.upper() == 'X': 
+                x *= scale_factor
+            elif axis.upper() == 'Y': 
+                y *= scale_factor
+            elif axis.upper() == 'Z': 
+                z *= scale_factor
+            else: 
+                raise ValueError("Axis must be 'X', 'Y', or 'Z'")
+                
+            result_points.append(Point([x, y, z]))
+        self.points = result_points
+        return self
+
+    def scale(self, scale_factor):
+        result_points = []
+        for point in self.points:
+            coords = point.coords if hasattr(point, 'coords') else point
+            new_coords = coords * scale_factor
+            result_points.append(Point(new_coords))
+        self.points = result_points
+        return self
